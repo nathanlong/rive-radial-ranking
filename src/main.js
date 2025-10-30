8fe8c1db5106f79692c6f942a17a62d476b1d6bb
Nathan Long <nlong5@gmail.com>
2025-10-30 15:28:00 -0400

    helps to bind the data model

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import * as rive from "@rive-app/canvas";
 
 const r = new rive.Rive({
   src: "/radial_ranking.riv",
-  // OR the path to a discoverable and public Rive asset
-  // src: '/public/example.riv',
   canvas: document.getElementById("radial"),
   autoplay: true,
-  // artboard: "Artboard", // Optional. If not supplied the default is selected
+  autoBind: true,
   stateMachines: "State Machine 1",
   onLoad: () => {
     r.resizeDrawingSurfaceToCanvas();
