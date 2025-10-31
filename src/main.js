fb07475008f8c7d8c3ec52e0e478903e15c19c4d
Nathan Long <nlong5@gmail.com>
2025-10-31 11:41:13 -0400

    Adding resets and trackers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import "./style.css";
 import * as rive from "@rive-app/canvas";
 
+let selectedZone, isRegistered, isSelected;
+
+const selectedText = document.querySelector("[data-selected-zone]");
+const resetButton = document.querySelector("[data-reset]");
+
 const r = new rive.Rive({
   src: "/radial_ranking.riv",
   canvas: document.getElementById("radial"),
@@ -9,5 +14,51 @@ const r = new rive.Rive({
   stateMachines: "State Machine 1",
   onLoad: () => {
     r.resizeDrawingSurfaceToCanvas();
+    const defaultVM = r.defaultViewModel();
+    const instance = r.viewModelInstance;
+    selectedZone = instance.number("selected_zone");
+    isRegistered = instance.boolean("is_registered");
+    isSelected = instance.boolean("is_selected");
+
+    selectedZone.on((event) => {
+      if (isRegistered.value === false && selectedZone.value !== 0) {
+        console.log(
+          "pre-change",
+          isRegistered.value,
+          isSelected.value,
+          selectedZone.value
+        );
+        isRegistered.value = true;
+        selectedText.innerHTML = parseInt(event);
+        resetButton.removeAttribute("disabled");
+        console.log(
+          "post-change",
+          isRegistered.value,
+          isSelected.value,
+          selectedZone.value
+        );
+      } else {
+        console.log(
+          "else",
+          isRegistered.value,
+          isSelected.value,
+          selectedZone.value
+        );
+      }
+    });
   },
+});
+
+resetButton.addEventListener("click", (e) => {
+  isRegistered.value = false;
+  isSelected.value = false;
+  selectedZone.value = 0;
+  resetButton.setAttribute("disabled", "true");
+  selectedText.innerHTML = parseInt(selectedZone.value)
+  console.log(
+    "reset",
+    isRegistered.value,
+    isSelected.value,
+    selectedZone.value
+  );
 });
